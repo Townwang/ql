bcd11b9bf5bb8cdbb6efa93177205f64dda71fe9
Townwang <android@townwang.com>
2026-04-29 01:08:48 +0800

    update yaohuo.py

--- scripts/games/yaohuo.py
+++ scripts/games/yaohuo.py
@@ -41,7 +41,7 @@ COOKIE = os.getenv("YH_COOKIE", "").strip()
 PASSWORD = os.getenv("YH_PASSWORD", "").strip()
 
 CONFIG = {
-    "base_bet": 5200,           # 基础投注
+    "base_bet": 200,           # 基础投注
     "max_bet": 5000000,        # 单次最大投注
     "max_network_errors": 10,  # 网络错误超限次数
     "request_retries": 5,      # 单接口请求重试次数
