57900c30c322dc777297834eae02e804bacd2c81
Townwang <android@townwang.com>
2026-05-15 20:21:46 +0800

    update yaohuo.py

--- scripts/games/yaohuo.py
+++ scripts/games/yaohuo.py
@@ -167,7 +167,7 @@ def get_win_provoke_words():
     # 用上一局挑战者，没有就默认陌生人
     nick = state.last_challenger if state.last_challenger else "神秘玩家"
     return [
-        f"😁😁上局赢了「{nick}」,嘻嘻😁😁"
+        f"😁上局赢了「{nick}」,嘻嘻😁😁"
     ]
 #    return [
 #        "还敢再来送吗？",
@@ -183,7 +183,7 @@ def get_lose_provoke_words():
     # 用上一局挑战者，没有就默认陌生人
     nick = state.last_challenger if state.last_challenger else "神秘玩家"
     return [
-        f"🙁🙁可恶的「{nick}」,不嘻嘻🙁🙁"
+        f"🙁可恶的「{nick}」,不嘻嘻🙁🙁"
     ]
 #    return [
 #        "刚才大意了，敢再来吗？",
